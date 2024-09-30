c2be09fb53201ff855e62f7a628ae7a35ed20f9b
TamaraGomezMic <119648127+TamaraGomezMic@users.noreply.github.com>
2024-09-30 19:17:33 -0300

    Update conexion.php
    cambios

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";  // El host donde está tu base de datos (normalmente localhost)
-$usuario = "root";    // El usuario de MySQL (normalmente root)
-$password = "";       // La contraseña de MySQL (si no tiene, deja en blanco)
-$base_datos = "pokedex";  // El nombre de tu base de datos
+$host = "localhost";  
+$usuario = "root";   
+$password = "";      
+$base_datos = "pokedex";  
 
-// Crear la conexión
+// Crea la conexión
 $conn = new mysqli($host, $usuario, $password, $base_datos);
 
-// Verificar la conexión
+// Verifica la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
